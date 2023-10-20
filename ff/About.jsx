2d69f737508ec4fd7172faf85c756184821bbf7d
IlyaKonovaloB <103986019+IlyaKonovaloB@users.noreply.github.com>
2023-10-20 12:18:08 +0300

    Add files via upload

---
+++ ff/About.jsx
@@ -0,0 +1,47 @@
+function About() {
+    return <>
+        <div className="backgr">
+            <div className="footer_gr">
+                <img src="./img/Group.png" alt="" />
+                <div>Производство</div>
+                <div>Достижения</div>
+                <div>Новости</div>
+                <div>Документация</div>
+                <div>Контакты</div>
+            </div>
+            <div className="qwe">О шоколаде</div>
+            <div className="woqe">
+                <img src="./img/image2.png" alt="" />
+                <div className="op_chok">На нашей фабрике шоколатье измельчают ингредиенты, чтобы шоколад буквально таял во рту. Чтобы добиться еще более нежной текстуры шоколада, мы час за часом медленно и тщательно перемешиваем нагретую шоколадную массу. В нашем шоколаде есть место только ингредиентам высочайшего качества. Красители, усилители вкуса и консерванты — это не наш путь.</div>
+            </div>
+        </div>
+        <div className="tyr">
+            <div className="tyu">
+                <div>Баунти</div>
+                <img src="./src/assets/img1.png" alt="...." />
+            </div>
+            <div className="tyu1">
+                <div>Баунти</div>
+                <img src="./src/assets/img2.png" alt="...." />
+            </div>
+            <div className="tyu2">
+                <div>Баунти</div>
+                <img src="./src/assets/img3.png" alt="...." />
+            </div>
+            <div className="tyu3">
+                <div>Баунти</div>
+                <img src="./src/assets/img4.png" alt="...." />
+            </div>
+            <div className="tyu4">
+                <div>Баунти</div>
+                <img src="./src/assets/img5.png" alt="...." />
+            </div>
+            <div className="tyu5">
+                <div>Баунти</div>
+                <img src="./src/assets/img6.png" alt="...." />
+            </div>
+        </div>
+
+    </>
+}
+export default About;
